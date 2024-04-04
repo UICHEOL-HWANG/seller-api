352fad08260c16e095e1d5faab1c6963caa48aad
UICHEOL-HWANG <icuchoel@gmail.com>
2024-04-04 16:01:48 +0900

    Fix: 출력내용 수정

--- api/router/llm/llm_call.py
+++ api/router/llm/llm_call.py
@@ -5,7 +5,8 @@ from .llm_schemas import *
 import os 
 
 # api key 환경변수 설정
-load_dotenv("/home/ubuntu/seller-api/api/router/llm/.env")
+# load_dotenv("/home/ubuntu/seller-api/api/router/llm/.env")
+load_dotenv(r"C:\Users\UICHEOL_HWANG\Desktop\seller\api\router\llm\.env")
 
 host_tmp = os.getenv('host_key')
 api_key_tmp= os.getenv('api')
@@ -95,8 +96,8 @@ async def generate_category(request: CategoryRequestModel):
     title_name = request.title
     
     
-    preset_text = [{"role":"system","content":"제품명과 블로그 제목을 통해 키워드 생성 \n제품명: 맥북\n블로그 제목: - 애플 노트북 추천, 맥북 에어 M1칩 리뷰\n키워드 추천:,맥북,맥북프로,아이패드중고,애플맥북,맥북중고"},{"role":"user","content":"제품명:밥솥\n블로그 제목:쿠쿠 압력밥솥 리뷰"},{"role":"assistant","content":"키워드 추천 : 밥솥, 전기밥솥, 쿠쿠밥솥, 압력밥솥추천, 3인용밥솥"},{"role":"user","content":"제품명: 샤오미\n블로그 제목:가성비 샤오미 스마트 밴드 스마트워치 추천, 미밴드8 사용기"},{"role":"assistant","content":"키워드 추천 : 샤오미, 스마트워치, 스마트밴드, 미밴드, 샤오미미밴드"}
-                   ,{"role":"user","content":f"제품명:{product_name}\n블로그 제목:{title_name}"}]
+    preset_text =[{"role":"system","content":"제품명과 블로그 제목을 통해 키워드 생성  키워드는 하이픈(-)으로 구분\n제품명: 맥북\n블로그 제목: -애플 노트북 추천, 맥북 에어 M1칩 리뷰\n키워드 추천: -맥북 -맥북프로 -아이패드중고,애플맥북 -맥북중고"},{"role":"user","content":"제품명:밥솥\n블로그 제목:쿠쿠 압력밥솥 리뷰"},{"role":"assistant","content":"키워드 추천 : -밥솥 -쿠쿠압력밥솥 -전기밥솥추천 -압력밥솥가격"},{"role":"user","content":"제품명:오렌지\n블로그 제목: 천혜향 맛있는 리뷰"},{"role":"assistant","content":"키워드 추천 : -오렌지 -천혜향 -한라봉 -황금향"}
+                  ,{"role":"user","content":f"제품명:{product_name}\n블로그 제목:{title_name}"}]
     request_data = {
         'messages': preset_text,
         'topP': 0.8,
