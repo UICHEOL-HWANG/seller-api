a8599bd6b91a605cfe8e1538fe9c26dc868a0f67
UICHEOL-HWANG <icuchoel@gmail.com>
2024-04-09 15:36:48 +0900

    Feat: 키워드 생성

--- api/main.py
+++ api/main.py
@@ -7,6 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from router.baseline.baseline import router as content_router
 from router.keyword.keyword import router as keywor_router
 from router.review_content.review_create import router as review_router
+from router.rekeyword.rekeyword import router as rekeyword_router
 
 app = FastAPI()
 app.add_middleware(
@@ -23,7 +24,7 @@ app.add_middleware(
 app.include_router(content_router)
 app.include_router(keywor_router)
 app.include_router(review_router)
-
+app.include_router(rekeyword_router)
 
 # Uvicorn으로 FastAPI 애플리케이션 실행 (개발용)
 if __name__ == '__main__':
---
+++ api/router/rekeyword/rekeyword.py
@@ -0,0 +1,30 @@
+# library 
+import hashlib,hmac,base64
+import time
+import json
+from fastapi import APIRouter
+import re 
+
+# Module 
+from .rekeyword_schemas import *
+from config.get_api import * 
+from config.crawl import * 
+
+# access_key
+host_tmp, api_key_tmp, api_key_primary_val_tmp, request_id_tmp = issued_keys()
+
+router = APIRouter(prefix="/re_keyword",
+                    tags=["rekeyword-section"],
+                   responses={404:{"description":"Not Found"}}
+                   )
+
+@router.post("/generate_rekeyowrd")
+async def generate_keyword(request: RekeywordRequests):
+    keyword = request.keyword
+    hintKeywords = [keyword]
+    
+    resultdf = getresults(hintKeywords)
+    resultdf['monthlyPcQcCnt'] = resultdf['monthlyPcQcCnt'].apply(lambda x: re.sub("<", "", x) if "<" in str(x) else x)
+    resultdic = resultdf.loc[:4, ["relKeyword", "monthlyPcQcCnt", "compIdx"]].to_dict()
+    
+    return resultdic
